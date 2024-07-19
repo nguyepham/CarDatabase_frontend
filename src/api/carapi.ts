7900b0d42b3b4552c2e6ecec5b72ac9820fca30a
nguyepham <nguyenminhpham24@gmail.com>
2024-07-19 10:49:21 +0700

    Added delete button

--- src/api/carapi.ts
+++ src/api/carapi.ts
@@ -2,6 +2,10 @@ import {CarResponse} from "../type.ts";
 import axios from "axios";
 
 export const getCars = async (): Promise<CarResponse[]> => {
-    const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`)
-    return response.data._embedded.cars
+    const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`)
+    return res.data._embedded.cars
+}
+
+export const deleteCar = async (link: string): Promise<void> => {
+    await axios.delete(link)
 }
--- src/components/Carlist.tsx
+++ src/components/Carlist.tsx
@@ -1,8 +1,10 @@
-import {useQuery} from "@tanstack/react-query";
-import {getCars} from "../api/carapi.ts";
-import {DataGrid, GridColDef} from "@mui/x-data-grid";
+import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
+import {deleteCar, getCars} from "../api/carapi.ts";
+import {DataGrid, GridCellParams, GridColDef} from "@mui/x-data-grid";
 
 function Carlist() {
+
+    const queryClient = useQueryClient()
 
     const columns: GridColDef[] = [
         {field: "brand", headerName: "Brand", width: 200},
@@ -11,11 +13,36 @@ function Carlist() {
         {field: "registrationNumber", headerName: "Reg.nr", width: 150},
         {field: "modelYear", headerName: "Model Year", width: 150},
         {field: "price", headerName: "Price", width: 150},
+        {
+            field: "delete",
+            headerName: "",
+            width: 90,
+            sortable: false,
+            filterable: false,
+            disableColumnMenu: true,
+            renderCell: (params: GridCellParams) => (
+                <button
+                    onClick={() => mutate(params.row._links.car.href)}>
+                    Delete
+                </button>
+            )
+        }
     ]
 
     const {data, error, isSuccess} = useQuery({
         queryKey: ["car"],
         queryFn: getCars
+    })
+
+    const {mutate} = useMutation({
+        mutationFn: deleteCar,
+        onSuccess: () => {
+            // Car deleted
+            queryClient.invalidateQueries({queryKey: ["car"]})
+        },
+        onError: (err) => {
+            console.log(err)
+        },
     })
 
     if (!isSuccess) {
@@ -29,6 +56,7 @@ function Carlist() {
             <DataGrid
                 rows={data}
                 columns={columns}
+                disableRowSelectionOnClick={true}
                 getRowId={row => row._links.self.href}
             />
         )
